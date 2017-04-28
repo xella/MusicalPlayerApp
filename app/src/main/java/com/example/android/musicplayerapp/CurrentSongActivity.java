4d2b4dfd61ec794892cd10069b6ca20254d9432d
Xella <xella.design@gmail.com>
2017-04-28 17:33:04 +0200

    first commit

---
+++ app/src/main/java/com/example/android/musicplayerapp/CurrentSongActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.musicplayerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentSongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_song);
+    }
+}
