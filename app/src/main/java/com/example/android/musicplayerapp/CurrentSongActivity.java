9ccb32adc5950cdda78613afb2887ab7b6f152ab
Xella <xella.design@gmail.com>
2017-05-03 18:15:00 +0200

    Added TextViews for Artist name and Song title. Added onClickListerner for play button. Added music file.

--- app/src/main/java/com/example/android/musicplayerapp/CurrentSongActivity.java
+++ app/src/main/java/com/example/android/musicplayerapp/CurrentSongActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayerapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CurrentSongActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CurrentSongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_song);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.piano);
+
+        ImageButton playButton = (ImageButton) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
     }
 }
