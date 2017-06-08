e09193fc4d9cfba5b2dba8bcb3a4866eef959048
Xella <xella.design@gmail.com>
2017-06-08 15:50:20 +0200

    Styles for list divider moved to styles.xml Current song layout changed. TextView with explanation added to main activity Intent to open current song activity from songs list added.

--- app/src/main/java/com/example/android/musicplayerapp/SongsActivity.java
+++ app/src/main/java/com/example/android/musicplayerapp/SongsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class SongsActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class SongsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
 
+        CardView song1= (CardView) findViewById(R.id.song_one);
 
+        song1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent playSongIntent = new Intent(SongsActivity.this, CurrentSongActivity.class);
+                startActivity(playSongIntent);
+            }
+        });
     }
 }
